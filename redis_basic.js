813dbe9ecf5990b9a4ff0fec9d66acb80bc1333b
zhuweili <weilizhu@buffalo.edu>
2016-08-17 15:31:30 -0400

    add redis support

--- redis_basic.js
+++ redis_basic.js
@@ -13,3 +13,20 @@ client.get('color', function(err, value) {
     if (err) throw err;
     console.log('Got:  ' + value);
 });
+
+client.hmset('camping', {
+    'shelter': '2-person tent',
+    'cooking': 'campstove'
+}, redis.print);
+
+client.hget('camping', 'cooking', function(err, value) {
+    if (err) throw err;
+    console.log('Will be cooking with: ' + value);
+});
+
+client.hkeys('camping', function(err, keys) {
+    if (err) throw err;
+    keys.forEach(function(key, i) {
+        console.log(' ' + key);
+    });
+});
